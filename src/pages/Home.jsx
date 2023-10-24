5a259bf0ee47e1c3d8a5ea8fb408612391be98ae
Elijah James <elijah.jamesmay14@gmail.com>
2023-10-24 12:59:55 +0300

    Added a loader spinner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,21 @@ import TrendingCoins from "../components/TrendingCoins";
 
 const Home = () => {
   const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const url =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&locale=en";
 
   useEffect(() => {
-    axios.get(url).then((res) => setCoins(res.data));
+    setLoading(true);
+    axios
+      .get(url)
+      .then((res) => setCoins(res.data))
+      .finally(() => setLoading(false));
   }, [url]);
   return (
     <div>
-      <CoinSearch coins={coins} />
+      <CoinSearch coins={coins} loading={loading} />
       <TrendingCoins />
     </div>
   );
